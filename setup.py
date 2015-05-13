a6e389ba2d2fd94c69175fd2155c128701161179
Max Brauer <max@transcode.de>
2015-05-13 16:21:55 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ tests_requires = [
     'isort==3.9.5',
     'pytest-django==2.7.0',
     'httpretty>=0.8.0,!=0.8.1,!=0.8.2,!=0.8.3,!=0.8.7,!=0.8.8',
+    'pytest-httpretty==0.2.0',
     'pytest-pythonpath==0.6',
     'pytest==2.6.4',
     'tox==1.9.0',
