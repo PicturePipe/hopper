a4c41dabe1eb942f94ba3a03cc1779aea81eeac9
René Muhl <rene@transcode.de>
2015-05-15 14:29:06 +0200

    Added detail view test and correct URL building

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -29,3 +29,14 @@ def test_list_view_post(client, form_data):
         content_type='application/json')
     response = client.post(url, data=form_data, content_type='application/json')
     assert response.status_code == 201
+
+
+@pytest.mark.httpretty
+@pytest.mark.django_db
+def test_list_view_detail(client, fixture, model):
+    url = reverse('api-detail', kwargs={'pk': model.id})
+    httpretty.register_uri(httpretty.GET, url,
+        body=fixture('base_form.json'), content_type='application/json')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.data['url'].startswith('http://')
