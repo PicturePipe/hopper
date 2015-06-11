df03d4e22974d4135c8e405f2407d195e6bd74de
René Muhl <rene@transcode.de>
2015-06-11 17:26:59 +0200

    Added obtain token view

--- hopper/config/urls.py
+++ hopper/config/urls.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.conf.urls.static import static
@@ -5,6 +6,7 @@ from django.contrib import admin
 from rest_framework import routers
 
 from api.views import FormDataViewSet
+from authentication.views import ObtainUserJSONWebToken
 
 router = routers.DefaultRouter()
 router.register(r'api', FormDataViewSet, base_name='api')
@@ -13,5 +15,8 @@ urlpatterns = patterns('',
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^auth/$', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^user-token-auth/', ObtainUserJSONWebToken.as_view(),
+        name='obtain_user_jwt_token'),
     url(r'^', include(router.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
