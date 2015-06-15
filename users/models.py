9981d4216999b26ab251106de1477e18fd347fe6
René Muhl <rene@transcode.de>
2015-06-15 14:45:52 +0200

    Created own user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+# encoding: utf-8
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.sites.models import Site
 from django.db import models
 
-# Create your models here.
+
+class HopperUser(AbstractBaseUser):
+    username = models.CharField(max_length=200)
+    is_master = models.BooleanField(default=False)
+    site = models.ForeignKey(Site)
+
+    class Meta:
+        unique_together = (("username", "site"),)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
