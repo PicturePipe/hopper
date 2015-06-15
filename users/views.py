ad6f5f67480328dce08597f8c28c7539a24fbaa1
René Muhl <rene@transcode.de>
2015-06-15 14:46:24 +0200

    Added view and its POST method

--- users/views.py
+++ users/views.py
@@ -1,12 +1,31 @@
 # encoding: utf-8
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_jwt.views import ObtainJSONWebToken
 
-from .serializers import UserJSONWebTokenSerializer
+from .serializers import UserSerializer
 
 
-class ObtainUserJSONWebToken(ObtainJSONWebToken):
+class CreateUserView(APIView):
     """
     API View that receives a POST with a user's username and password.
     Returns a JSON Web Token that can be used for authenticated requests.
     """
-    serializer_class = UserJSONWebTokenSerializer
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        data = request.data
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response_data = serializer.data
+            token = ObtainJSONWebToken.as_view()(
+                request,
+                {'username': data['username'], 'password': data['password']}
+            )
+            response_data.update(dict(token=token))
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
