248bdc78978bcc58603a16de3b422ea5cc71275a
René Muhl <rene@transcode.de>
2015-06-18 18:18:31 +0200

    Changed user model

--- users/models.py
+++ users/models.py
@@ -1,16 +1,25 @@
 # encoding: utf-8
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.contrib.sites.models import Site
 from django.db import models
 
 
 class HopperUser(AbstractBaseUser):
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
+    email = models.EmailField('email address', blank=True)
     is_master = models.BooleanField(default=False)
     site = models.ForeignKey(Site)
+    is_staff = models.BooleanField('staff status', default=False,
+        help_text='Designates whether the user can log into this admin '
+                    'site.')
+    is_active = models.BooleanField('active', default=True,
+        help_text='Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.')
+
+    objects = UserManager()
 
     class Meta:
         unique_together = (("username", "site"),)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['email']
