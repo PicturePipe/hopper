03d90d78b47a0c22a8235e304e82d54f50d1ceda
René Muhl <rene@transcode.de>
2015-06-19 11:05:17 +0200

    Cleaned up tests

--- tests/users/test_auth.py
+++ tests/users/test_auth.py
@@ -5,21 +5,6 @@ import pytest
 from django.core.urlresolvers import reverse
 
 
-def test_obtain_token_for_existing_user(client, user, credentials):
-    url = reverse('obtain_jwt')
-    email, password = credentials
-    data = {'email': email, 'password': password}
-    response = client.post(url, data=json.dumps(data), content_type='application/json')
-    assert response.status_code == 200
-    assert response.data.get('token')
-
-
-def test_login_with_token(client, user, token):
-    url = reverse('api-root')
-    response = client.get(url, HTTP_AUTHORIZATION='JWT {0}'.format(token(user)))
-    assert response.status_code == 200
-
-
 @pytest.mark.django_db
 def test_create_user_with_wrong_token(client, user, token):
     url = reverse('create_user')
@@ -30,7 +15,7 @@ def test_create_user_with_wrong_token(client, user, token):
 
 
 @pytest.mark.django_db
-def test_create_user_bad_request(client, user, token):
+def test_create_master_user(client, user, token):
     # It is not allowed to create master user via API
     url = reverse('create_user')
     data = {
@@ -49,3 +34,18 @@ def test_create_user_with_master_token(client, master_user, token):
     response = client.post(url, data=json.dumps(data), content_type='application/json',
         HTTP_AUTHORIZATION='JWT {0}'.format(token(master_user)))
     assert response.status_code == 201
+
+
+def test_obtain_token_for_existing_user(client, user, credentials):
+    url = reverse('obtain_jwt')
+    email, password = credentials
+    data = {'email': email, 'password': password}
+    response = client.post(url, data=json.dumps(data), content_type='application/json')
+    assert response.status_code == 200
+    assert response.data.get('token')
+
+
+def test_login_with_token(client, user, token):
+    url = reverse('api-root')
+    response = client.get(url, HTTP_AUTHORIZATION='JWT {0}'.format(token(user)))
+    assert response.status_code == 200
