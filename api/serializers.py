f6c2258242510bb3b0faee263214ae7775fc24c8
René Muhl <rene@transcode.de>
2015-10-20 17:08:44 +0200

    Cleaned up tests

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
@@ -42,7 +42,7 @@ class FormDataSerializer(serializers.HyperlinkedModelSerializer):
         if self.instance:
             return_data['author'] = self.instance.author
         elif 'author' in data:
-            return_data['author'] = User.objects.get(pk=data['author'])
+            return_data['author'] = get_user_model().objects.get(pk=data['author'])
         form_data = data.pop('form', None)
         form_data_keys = ['method', 'action', 'enctype', 'title', 'help_text', 'css_classes',
             'elements_css_classes', 'elements']
--- tests/api_tests/test_views.py
+++ tests/api_tests/test_views.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 @pytest.mark.httpretty
 def test_get_api_root(client):
     url = reverse('api-root')
-    httpretty.register_uri(httpretty.GET, url, content_type='application/json')
+    httpretty.register_uri(httpretty.GET, url)
     get_response = client.get(url)
     assert get_response.status_code == 200
 
@@ -16,8 +16,7 @@ def test_get_api_root(client):
 @pytest.mark.django_db
 def test_list_view_get(client, fixture):
     url = reverse('api-list')
-    httpretty.register_uri(httpretty.GET, url,
-        body=fixture('base_form.json'), content_type='application/json')
+    httpretty.register_uri(httpretty.GET, url, body=fixture('base_form.json'))
     get_response = client.get(url)
     assert get_response.status_code == 200
 
@@ -26,8 +25,7 @@ def test_list_view_get(client, fixture):
 @pytest.mark.django_db
 def test_list_view_post(client, form_data):
     url = reverse('api-list')
-    httpretty.register_uri(httpretty.POST, url, body=form_data,
-        content_type='application/json')
+    httpretty.register_uri(httpretty.POST, url, body=form_data, content_type='application/json')
     response = client.post(url, data=form_data, content_type='application/json')
     assert response.status_code == 201
 
@@ -36,8 +34,7 @@ def test_list_view_post(client, form_data):
 @pytest.mark.django_db
 def test_list_view_detail(client, fixture, model):
     url = reverse('api-detail', kwargs={'pk': model.id})
-    httpretty.register_uri(httpretty.GET, url,
-        body=fixture('base_form.json'), content_type='application/json')
+    httpretty.register_uri(httpretty.GET, url, body=fixture('base_form.json'))
     response = client.get(url)
     assert response.status_code == 200
     assert response.data['url'].startswith('http://')
